f149ca45b41279ddd8fb2ee2dcbcc383a96f7549
DinnerWithoutTomato <xqiutongxue@foxmail.com>
2025-02-03 12:51:59 +0800

    feat: 为更新功能添加 logger

--- electron/main/update.ts
+++ electron/main/update.ts
@@ -6,15 +6,18 @@ import type {
 } from 'electron-updater'
 import { createRequire } from 'node:module'
 import { app, ipcMain } from 'electron'
+import { createLogger } from './logger'
 
 const { autoUpdater }: { autoUpdater: AppUpdater } = createRequire(import.meta.url)('electron-updater')
 
 export function update(win: Electron.BrowserWindow) {
+  const logger = createLogger('update')
   // When set to false, the update download will be triggered through the API
   autoUpdater.autoDownload = false
   autoUpdater.disableWebInstaller = false
   autoUpdater.allowDowngrade = false
-
+  // TODO: 设一个本地服务器模拟一下
+  // autoUpdater.forceDevUpdateConfig = true
   // start check
   autoUpdater.on('checking-for-update', () => { })
   // update available
@@ -37,6 +40,8 @@ export function update(win: Electron.BrowserWindow) {
       return await autoUpdater.checkForUpdatesAndNotify()
     }
     catch (error) {
+      if (error instanceof Error)
+        logger.error(error.message)
       return { message: '网络错误', error }
     }
   })
@@ -46,6 +51,7 @@ export function update(win: Electron.BrowserWindow) {
     startDownload(
       (error, progressInfo) => {
         if (error) {
+          logger.error('下载错误: ', error.message)
           // feedback download error message
           event.sender.send('update-error', { message: error.message, error })
         }
