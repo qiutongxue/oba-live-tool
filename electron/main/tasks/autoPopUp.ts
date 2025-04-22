7f2f056611dc77d410ec2119c76cf430a8f6d4cc
DinnerWithoutTomato <xqiutongxue@foxmail.com>
2025-04-22 16:23:50 +0800

    feat: 新增视频号的自动弹窗和自动发言

--- electron/main/tasks/autoPopUp.ts
+++ electron/main/tasks/autoPopUp.ts
@@ -138,10 +138,32 @@ class PopUpManager {
 function setupIpcHandlers() {
   typedIpcMainHandle(IPC_CHANNELS.tasks.autoPopUp.start, async (_, config) => {
     try {
-      pageManager.register(
-        TASK_NAME,
-        (page, account) => new PopUpManager(page, account, config),
-      )
+      // 视频号的商品弹窗不在中控台页面，需要单独处理
+      if (pageManager.getContext()?.platform === 'wxchannel') {
+        const browserContext = pageManager.getContext()?.browserContext
+        if (!browserContext) throw new Error('无法获取浏览器上下文')
+        // 先判断是否有商品管理页面
+        let popUpPage = browserContext
+          .pages()
+          .find(page => page.url().includes('commodity/onsale/index'))
+
+        // 没有就创建一个
+        if (!popUpPage) {
+          popUpPage = await browserContext.newPage()
+          await popUpPage.goto(
+            'https://channels.weixin.qq.com/platform/live/commodity/onsale/index',
+          )
+        }
+        pageManager.register(
+          TASK_NAME,
+          (_, account) => new PopUpManager(popUpPage, account, config),
+        )
+      } else {
+        pageManager.register(
+          TASK_NAME,
+          (page, account) => new PopUpManager(page, account, config),
+        )
+      }
       pageManager.startTask(TASK_NAME)
       return true
     } catch (error) {
