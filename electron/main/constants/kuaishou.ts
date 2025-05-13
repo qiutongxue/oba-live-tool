e8e9d17d2386aeb7d77021b8e551313988831ec5
DinnerWithoutTomato <xqiutongxue@foxmail.com>
2025-05-13 11:28:58 +0800

    feat: 新增快手小店的自动弹窗和自动发言

--- electron/main/constants/kuaishou.ts
+++ electron/main/constants/kuaishou.ts
@@ -9,3 +9,28 @@ export const login: LoginConstants = {
   isLoggedInSelector: '[class^=nickname] [class^=name]',
   isInLiveControlSelector: 'div[class^=live-panel]',
 }
+
+export const selectors = {
+  GOODS_ITEM: '[class^=list-container] [class^=list-item]',
+  GOODS_ITEMS_WRAPPER: '[class^=list-container] [data-virtuoso-scroller]',
+  goodsItem: {
+    ID: 'input',
+    POPUP_BUTTON: '[class^=cardBtn] > button:last-child',
+    // 若有正在弹窗的商品，不管是结束弹窗还是开始弹窗都会弹出一个 modal
+    // 需要点击 modal 上的确认按钮才能执行下一步操作
+    POPUP_CONFIRM_BUTTON: '.ant-modal-root button:last-child',
+  },
+  CLOSE_VIDEO_BTN: '[class^=live-panel] [class^=btn] > div:first-child',
+  VIDEO: '[class^=video-container]',
+  commentInput: {
+    TEXTAREA: 'div[class^=reply-content] input[type=text]',
+    PIN_TOP_LABEL: 'div[class^=reply-content] label',
+    SUBMIT_BUTTON: 'div[class^=reply-content] button',
+  },
+  overlays: {
+    // 刚进中控台弹出的：团购模式全新上线，选择暂不切换（第一个按钮）
+    SWITCH_TO_GROUP_BUYING: '.ant-modal-root button',
+    // 开启直播后，中控台会弹出窗口：已开启直播，选择我知道了（唯一一个按钮（
+    LIVE_ON: '.ant-modal-root button',
+  },
+} as const
--- electron/main/tasks/controller/PopUpStrategy.ts
+++ electron/main/tasks/controller/PopUpStrategy.ts
@@ -1,46 +1,84 @@
-import type { ElementHandle } from 'playwright'
+import type { ElementHandle, Page } from 'playwright'
+import { kuaishou } from '#/constants'
+import { createLogger } from '#/logger'
 import { sleep } from '#/utils'
 
 export type PopUpStrategy = (
   element: ElementHandle<SVGElement | HTMLElement>,
+  page: Page,
+  getButton: () => Promise<ElementHandle<SVGElement | HTMLElement>>,
 ) => Promise<void>
+
+const logger = createLogger('弹窗')
 
 const redbookPopUpStrategy: PopUpStrategy = async element => {
   await element.click()
 }
 
-const douyinPopUpStrategy: PopUpStrategy = async button => {
-  const clickPopUpButton = async (
-    button: ElementHandle<SVGElement | HTMLElement>,
-  ) => {
-    const buttonText = await button.textContent()
-    if (buttonText !== '取消讲解' && buttonText !== '讲解') {
-      throw new Error(`不是讲解按钮，是 ${buttonText} 按钮`)
-    }
-    await button.dispatchEvent('click')
-    return buttonText
-  }
-
-  while ((await clickPopUpButton(button)) === '取消讲解') {
-    await sleep(1000)
-  }
-}
-
-const wxchannelPopUpStrategy: PopUpStrategy = async button => {
+const togglePopUp = async (
+  button: ElementHandle<SVGElement | HTMLElement>,
+  promote: string,
+  cancelPromote: string,
+) => {
   const clickPopUpButton = async (
     button: ElementHandle<SVGElement | HTMLElement>,
   ) => {
     const buttonText = (await button.textContent())?.trim()
-    if (buttonText !== '结束讲解' && buttonText !== '讲解') {
-      throw new Error(`不是讲解按钮，是 ${buttonText} 按钮`)
+    if (buttonText !== cancelPromote && buttonText !== promote) {
+      throw new Error(`不是${promote}按钮，是 ${buttonText} 按钮`)
     }
     await button.dispatchEvent('click')
     return buttonText
   }
 
-  while ((await clickPopUpButton(button)) === '结束讲解') {
+  while ((await clickPopUpButton(button)) === cancelPromote) {
     await sleep(1000)
   }
+}
+
+const douyinPopUpStrategy: PopUpStrategy = async button => {
+  await togglePopUp(button, '讲解', '取消讲解')
+}
+
+const wxchannelPopUpStrategy: PopUpStrategy = async button => {
+  await togglePopUp(button, '讲解', '结束讲解')
+}
+
+const kuaishouPopUpStrategy: PopUpStrategy = async (
+  button,
+  page,
+  getButton,
+) => {
+  const buttonText = (await button.textContent())?.trim()
+  if (buttonText !== '结束讲解' && buttonText !== '开始讲解') {
+    throw new Error(`不是讲解按钮，是 ${buttonText} 按钮`)
+  }
+  await button.dispatchEvent('click')
+  // 判断是否会出现 modal
+  const modalNextButton = await page
+    .waitForSelector(kuaishou.selectors.goodsItem.POPUP_CONFIRM_BUTTON, {
+      timeout: 500,
+    })
+    .catch(() => null)
+  // 出现了就点击“确定”
+  if (modalNextButton) {
+    await modalNextButton.dispatchEvent('click')
+  }
+
+  // 结束讲解 - 需要再次开始讲解
+  if (buttonText === '结束讲解') {
+    logger.debug('已结束原来的讲解')
+    // FIXME: 是否为了以防万一需要等待一段时间？
+    await sleep(100)
+    // 注意：此时无法使用原先的按钮，需要重新查找
+    const newButton = await getButton()
+    const newButtonText = await newButton.textContent()
+    if (newButtonText !== '开始讲解') {
+      throw new Error(`无法开始新的讲解，讲解按钮为：${newButtonText}`)
+    }
+    newButton.dispatchEvent('click')
+  }
+  logger.debug('开始新的讲解')
 }
 
 export function getPopUpStrategy(platform: LiveControlPlatform): PopUpStrategy {
@@ -49,7 +87,11 @@ export function getPopUpStrategy(platform: LiveControlPlatform): PopUpStrategy {
       return redbookPopUpStrategy
     case 'wxchannel':
       return wxchannelPopUpStrategy
-    default:
+    case 'kuaishou':
+      return kuaishouPopUpStrategy
+    case 'buyin':
+    case 'douyin':
+    case 'eos':
       return douyinPopUpStrategy
   }
 }
