d12df402fd2c6220b5deb44553bf0e174d885fac
DinnerWithoutTomato <xqiutongxue@foxmail.com>
2025-05-22 02:50:37 +0800

    chore: 更新发布脚本以自动化版本发布流程

---
+++ scripts/release.js
@@ -0,0 +1,35 @@
+import { readFile } from 'node:fs/promises'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { x } from 'tinyexec'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const pkgPath = resolve(__dirname, '../package.json')
+
+const pkg = JSON.parse(await readFile(pkgPath, 'utf-8'))
+const version = pkg.version
+
+const commitMessage = `chore: release v${version}`
+
+async function gitCommit() {
+  await x('git', ['add', 'CHANGELOG.md', 'package.json'], {
+    throwOnError: true,
+  })
+  await x('git', ['commit', '-m', commitMessage], { throwOnError: true })
+}
+
+async function gitTag() {
+  const tagName = `v${version}`
+  await x('git', ['tag', '--annotate', '--message', commitMessage, tagName], {
+    throwOnError: true,
+  })
+}
+
+async function gitPush() {
+  await x('git', ['push'], { throwOnError: true })
+  await x('git', ['push', '--tags'], { throwOnError: true })
+}
+
+gitCommit()
+  .then(() => gitTag())
+  .then(() => gitPush())
