217911a0215833f9598dc03dff8d9a8b3ae84cde
驼铃电商社 <93114244+TLS-802@users.noreply.github.com>
2025-05-27 17:30:29 +0800

    Update InstructionsCard.tsx

--- src/pages/LiveControl/components/InstructionsCard.tsx
+++ src/pages/LiveControl/components/InstructionsCard.tsx
@@ -10,7 +10,6 @@ import React from 'react'
 const instructions = [
   '选择平台并点击"连接直播控制台"按钮，等待登录',
   '登录成功后，即可使用自动发言和自动弹窗功能',
-  '自动回复功能目前仅对抖音小店和巨量百应开放',
 ]
 
 const InstructionsCard = React.memo(() => (
