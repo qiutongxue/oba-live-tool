1d20e409db7c6b2fb38ddcebf9d9f513f541ea04
DinnerWithoutTomato <xqiutongxue@foxmail.com>
2025-06-01 23:55:03 +0800

    refactor: 重构 utils 结构

---
+++ electron/main/utils/types.ts
@@ -0,0 +1,6 @@
+export type PromiseReturnType<T> = T extends Promise<infer U> ? U : T
+
+export type RequiredWith<BaseType, Keys extends keyof BaseType> = Required<
+  Pick<BaseType, Keys>
+> &
+  Omit<BaseType, Keys>
