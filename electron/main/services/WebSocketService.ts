da26e43a602ead71465bbaa8aa66cb7bae7c0b66
DinnerWithoutTomato <xqiutongxue@foxmail.com>
2025-11-19 12:42:19 +0800

    fix: 修复评论监听和WebSocket无法正常关闭的问题 #231
    Fixes #231

--- electron/main/services/WebSocketService.ts
+++ electron/main/services/WebSocketService.ts
@@ -12,24 +12,38 @@ export class WebSocketService {
   }
 
   start(port: number) {
-    let _port = port
-    if (!port || Number.isNaN(port) || port < 0 || !Number.isInteger(port)) {
-      _port = DEFAULT_PORT
-    }
-    if (this.wss) {
-      this.logger.warn('WebSocket服务已在运行')
-      return
-    }
+    return new Promise<void>((resolve, reject) => {
+      let _port = port
+      if (!port || Number.isNaN(port) || port < 0 || !Number.isInteger(port)) {
+        _port = DEFAULT_PORT
+      }
+      if (this.wss) {
+        this.logger.warn('WebSocket服务已在运行')
+        return resolve()
+      }
+      const server = new WebSocketServer({ port: _port })
 
-    this.wss = new WebSocketServer({ port: _port })
-    this.wss.on('connection', ws => {
-      this.logger.info('客户端已连接')
-      ws.on('close', () => {
-        this.logger.info('客户端已断开')
+      const startErrorHandler = (err: Error) => {
+        this.logger.error('WebSocket服务启动失败', err)
+        this.wss = null
+        server.close()
+        reject(err)
+      }
+      // 如果端口被占用会报错
+      server.once('error', startErrorHandler)
+      server.once('listening', () => {
+        this.logger.success(`WebSocket服务已启动，端口: ${_port}`)
+        server.off('error', startErrorHandler)
+        resolve()
       })
+      server.on('connection', ws => {
+        this.logger.info('客户端已连接')
+        ws.on('close', () => {
+          this.logger.info('客户端已断开')
+        })
+      })
+      this.wss = server
     })
-
-    this.logger.success(`WebSocket服务已启动，端口: ${_port}`)
   }
 
   broadcast<T>(data: T) {
